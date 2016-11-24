97268470afe842bd4cee1570fcf63b984a9fdf1d
hi618102 <hi1002@aliyun.com@4fca09e3-6650-4c34-907b-b3a543968c96>
2016-11-24 13:07:49 +0000

    Change-Id: Icec5c2fbde1593a0cb4b348b11153f5bf1c0f0ed

---
+++ Vacomall-Spring-RestApi/src/main/java/com/vacomall/aspect/ExceptionLogAdvice.java
@@ -0,0 +1,49 @@
+package com.vacomall.aspect;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+/**
+ * 异常日志记录
+ * @author Administrator
+ *
+ */
+@Aspect
+@Component
+public class ExceptionLogAdvice {
+	
+	public static final Logger logger = Logger.getLogger(ExceptionLogAdvice.class);
+	
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
+	public void doAfterThrowing() {
+		
+	}
+	
+	/**
+	 * 当系统抛出异常后执行
+	 * @param joinPoint
+	 */
+	@AfterThrowing( value = "doAfterThrowing()",throwing = "e")
+	public void doAfterThrowing(JoinPoint jp, Throwable e)  throws Throwable{
+		
+		MethodSignature methodSignature = (MethodSignature) jp.getSignature();
+		Method method = methodSignature.getMethod();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("className", jp.getTarget().getClass().getName());
+		map.put("methodName", method.getName());
+		map.put("args",new ObjectMapper().writeValueAsString(jp.getArgs()));
+		map.put("errorMsg",e.getMessage());
+		logger.error(new ObjectMapper().writeValueAsString(map));
+	}
+}
