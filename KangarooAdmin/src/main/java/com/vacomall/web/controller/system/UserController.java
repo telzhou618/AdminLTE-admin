3f009605db2cc538862a25db1df9423b4a2f9554
hi618102 <hi1002@aliyun.com@4fca09e3-6650-4c34-907b-b3a543968c96>
2016-12-09 06:49:54 +0000

    Change-Id: I41c017842997f3d4c5d1f9f2727d2b46fd030393

---
+++ KangarooAdmin/src/main/java/com/vacomall/web/controller/system/UserController.java
@@ -0,0 +1,21 @@
+package com.vacomall.web.controller.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * 用户控制器
+* @ClassName: UserController
+* @author Gaojun.Zhou
+* @date 2016年12月9日 下午1:48:35
+*
+ */
+@Controller
+@RequestMapping("/system/role")
+public class UserController {  
+
+    @RequestMapping("/list")  
+    public  String list(Model model){
+		return "system/user/list";
+    }  
+}
