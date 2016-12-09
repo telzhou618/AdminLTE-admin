ff08fecad9e40c1165d8a37d9146cd6dd7eb3e1d
hi618102 <hi1002@aliyun.com>
2016-12-09 09:52:50 +0000

    Change-Id: Ic605b4fcfdc41a15cee8ebec34d45b5d1d0d1c81

--- KangarooAdmin/src/main/java/com/vacomall/web/controller/system/RoleController.java
+++ KangarooAdmin/src/main/java/com/vacomall/web/controller/system/RoleController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 *
  */
 @Controller
-@RequestMapping("/system/user")
+@RequestMapping("/system/role")
 public class RoleController {  
 
     @RequestMapping("/list")  
