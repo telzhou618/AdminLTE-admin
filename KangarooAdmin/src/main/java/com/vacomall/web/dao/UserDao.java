f4d6cef9bf8344405c42f45e3244e30dd48bdba0
hi618102 <hi1002@aliyun.com>
2016-12-10 02:53:33 +0000

    Change-Id: I2bb7cdb59b0c5251afb65cf4c5b83793253c6b8f

---
+++ KangarooAdmin/src/main/java/com/vacomall/web/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.vacomall.web.dao;
+
+import com.baomidou.hibernateplus.dao.IDao;
+import com.vacomall.web.model.SysUser;
+
+/**
+ * <p>
+ * Dao类
+ * </p>
+ *
+ * @author gaojun.zhou
+ * @since 2016-12-09
+ */
+public interface UserDao extends IDao<SysUser> {
+
+}
---
+++ KangarooAdmin/src/main/java/com/vacomall/web/dao/impl/UserDaoImpl.java
@@ -0,0 +1,21 @@
+package com.vacomall.web.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.baomidou.hibernateplus.dao.impl.DaoImpl;
+import com.vacomall.web.dao.UserDao;
+import com.vacomall.web.model.SysUser;
+
+/**
+ * <p>
+ *   Dao实现类
+ * </p>
+ *
+ * @author gaojun.zhou
+ * @since 2016-12-09
+ */
+@Repository
+public class UserDaoImpl extends DaoImpl<SysUser> implements UserDao {
+
+
+}
