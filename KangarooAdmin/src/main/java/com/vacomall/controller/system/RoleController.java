e21fbe10ea0f2a8debda00adade7e04ec116a7cc
hi618102 <hi1002@aliyun.com>
2016-12-12 10:25:14 +0000

    Change-Id: I6a3c24a506ff46503e4348bcff8aaa95767a2345

--- KangarooAdmin/src/main/java/com/vacomall/controller/system/RoleController.java
+++ KangarooAdmin/src/main/java/com/vacomall/controller/system/RoleController.java
@@ -1,12 +1,18 @@
 package com.vacomall.controller.system;
 
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.vacomall.common.bean.Response;
 import com.vacomall.entity.SysRole;
@@ -25,13 +31,18 @@ public class RoleController {
 	@Autowired private ISysRoleService sysRoleService;
 	
     @RequestMapping("/list/{pageNumber}")  
-    public  String list(@PathVariable Integer pageNumber,Model model){
+    public  String list(@PathVariable Integer pageNumber,String search,Model model){
     	
 		Page<SysRole> page = new Page<SysRole>(pageNumber,20);
 		page.setOrderByField("createTime");
 		page.setAsc(false);
 		// 查询分页
-		Page<SysRole> pageData = sysRoleService.selectPage(page, null);
+		EntityWrapper<SysRole> ew = new EntityWrapper<SysRole>();
+		if(StringUtils.isNotBlank(search)){
+			ew.like("roleName",search);
+			model.addAttribute("search",search);
+		}
+		Page<SysRole> pageData = sysRoleService.selectPage(page, ew);
 		model.addAttribute("pageData", pageData);
 		return "system/role/list";
     } 
@@ -44,6 +55,7 @@ public class RoleController {
     
     @RequestMapping("/doAdd")  
     public  String doAdd(SysRole role){
+    	role.setCreateTime(new Date());
     	sysRoleService.insertSelective(role);
 		return "redirect:/system/role/list/1.html";
 
@@ -53,6 +65,21 @@ public class RoleController {
     @ResponseBody
     public  Response delete(String id){
     	sysRoleService.deleteById(id);
+    	return new Response().success();
+    }  
+    
+    /**
+     * 批量删除
+    * @Title: deleteBatch 
+    * @param @param id
+    * @param @return     
+    * @return Response     
+    * @throws
+     */
+    @RequestMapping("/deleteBatch")  
+    @ResponseBody
+    public Response deleteBatch(@RequestParam("id[]") List<String> ids){
+    	sysRoleService.deleteBatchIds(ids);
     	return new Response().success();
     }  
     
--- KangarooAdmin/src/main/webapp/WEB-INF/static/dist/js/init.js
+++ KangarooAdmin/src/main/webapp/WEB-INF/static/dist/js/init.js
@@ -15,6 +15,7 @@ $("*[data-tiggle='ajax']").click(function(){
 	bootbox.confirm({
 	    title: "警告消息",
 	    message: dataConfirm ? dataConfirm : "确认执行该操作?",
+	    backdrop: true,
 	    buttons: {
 	        cancel: {
 	            label: '<i class="fa fa-times"></i> 取消'
@@ -37,5 +38,70 @@ $("*[data-tiggle='ajax']").click(function(){
 	
 });
 
-  
+
+//icheck
+
+var checkedBgColor = "#f5f5f5",unCheckedBgColor = "#fff";
+
+$(".checkbox-item").on('ifChecked',function(){
+	$(this).parent().parent().parent().css('background',checkedBgColor);
+});
+
+$(".checkbox-item").on('ifUnchecked',function(){
+	$(this).parent().parent().parent().css('background',unCheckedBgColor);
+});
+$(".checkbox-toolbar").on('ifChecked',function(){
+	$(".checkbox-item").iCheck('check');
+	$(".checkbox-item").parent().parent().parent().css('background',checkedBgColor);
+	
+});
+$(".checkbox-toolbar").on('ifUnchecked',function(){
+	$(".checkbox-item").iCheck('uncheck');
+	$(".checkbox-item").parent().parent().parent().css('background',unCheckedBgColor);
+	
+});
+
+
+//delete batch
+
+$("*[delete-batch-url]").click(function(){
+	var deleteBatchUrl = $(this).attr('delete-batch-url');
+	var ids = [];
+	$.each($("input:checked"),function(n,i){
+		ids.push($(this).val());
+	});
+	
+	if(ids.length==0){
+		bootbox.alert({
+			title:'提示',
+		    message: "请选择要删除的记录!",
+		    backdrop: true
+		});
+	}else{
+		bootbox.confirm({
+	    title: "警告消息",
+	    message: "确认删除选中的【"+ids.length+"】条记录?",
+	    backdrop: true,
+	    buttons: {
+	        cancel: {
+	            label: '<i class="fa fa-times"></i> 取消'
+	        },
+	        confirm: {
+	            label: '<i class="fa fa-check"></i> 确认',
+	             className: 'btn-success'
+	        }
+	    },
+	    callback: function (result) {
+	    	if(result){
+	    		$.post(deleteBatchUrl,{id:ids},function(json){
+	    			if(json.meta.success){
+	    				window.location.reload();
+	    			}
+	    		});
+	    	}
+	    }
+	});
+	}
+});
+
 })(jQuery, window);
