fec1ecea299268bbfe9369307b36f81354483dbf
hi618102 <hi1002@aliyun.com@4fca09e3-6650-4c34-907b-b3a543968c96>
2016-12-20 10:02:49 +0000

    修改密码

--- KangarooAdmin/src/main/java/com/vacomall/controller/system/MeController.java
+++ KangarooAdmin/src/main/java/com/vacomall/controller/system/MeController.java
@@ -1,5 +1,6 @@
 package com.vacomall.controller.system;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.baomidou.kisso.SSOHelper;
 import com.vacomall.common.controller.SuperController;
+import com.vacomall.common.util.CommonUtil;
 import com.vacomall.entity.SysUser;
 import com.vacomall.service.ISysUserService;
 /**
@@ -27,5 +29,38 @@ public class MeController extends SuperController{
     	model.addAttribute("sysUser", sysUser);
 		return "system/me/page";
     } 
+    
+    /**
+     * 修改密码
+     */
+    @RequestMapping("/doChangePwd")
+    public String doChangePwd(String password,String newpassword,String newpassword2,Model model){
+    	
+    	if(StringUtils.isBlank(password) || StringUtils.isBlank(newpassword) || StringUtils.isBlank(newpassword2)){
+    		model.addAttribute("msg","客户端提交数据不能为空.");
+    		model.addAttribute("act","2");
+    		return "system/me/page";
+    	}
+    	
+    	SysUser user = sysUserService.selectById(SSOHelper.getToken(request).getUid());
+    	if(!user.getPassword().equals(CommonUtil.MD5(password))){
+    		model.addAttribute("msg","旧密码输入错误.");
+    		model.addAttribute("act","2");
+    		return "system/me/page";
+    	}
+    	
+    	if(!newpassword2.equals(newpassword)){
+    		model.addAttribute("msg","两次输入的密码不一致.");
+    		model.addAttribute("act","2");
+    		return "system/me/page";
+    	}
+    	
+    	user.setPassword(CommonUtil.MD5(newpassword));
+    	sysUserService.updateById(user);
+    	
+    	model.addAttribute("info","密码修改成功.");
+		model.addAttribute("act","2");
+		return "system/me/page";
+    }
     
 }
